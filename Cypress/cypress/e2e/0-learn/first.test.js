d06aad17af6fd507881d6ce7854aa5f3bbc70cdc
userabid <abid.ubaidillah2707@gmail.com>
2024-05-16 22:29:41 +0700

    Memasukan Folder Cypress

---
+++ Cypress/cypress/e2e/0-learn/first.test.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+    it('Visit the Kitchen sink', () => {
+     
+        cy.visit('https://example.cypress.io')
+        cy.get('h1').contains('Kitchen Sink')
+        cy.contains('type').click()
+
+        cy.url().should, 'include', '/commands/actions'
+
+        cy.get('.action-email').type('learn@email.com')
+        cy.get('.action-email').should('have.value', 'learn@email.com')
+
+    });
+})
