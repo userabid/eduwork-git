a8a3aebf6af2916baa84a30b3007fd84cb444920
userabid <abid.ubaidillah2707@gmail.com>
2024-05-16 22:53:24 +0700

    Update Tugas

---
+++ Cypress/cypress/e2e/0-learn/first.test.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+//Tugas Test Script
+describe('My First Test', () => {
+  it('Visit the Kitchen sink', () => {
+   
+      cy.visit('https://example.cypress.io')
+      cy.get('h1').contains('Kitchen Sink')
+      cy.contains('type').click()
+
+      cy.url().should, 'include', '/commands/actions'
+
+      cy.get('.action-email').type('learn@email.com')
+      cy.get('.action-email').should('have.value', 'learn@email.com')
+
+  });
+})
